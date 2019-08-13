80cc1bcd07929f58a577c9a5b31b56029a6d3de0
Teatoller <steven.otieno1@gmail.com>
2019-08-13 21:26:01 +0300

    (separation): implement separation of view and logic
    - create file for views
    - create file for logic
    
    [Starts #003]

--- index.php
+++ index.php
@@ -1,25 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
+$greeting = "Hello, World";
 
-<head>
-    <title>Document</title>
-    <meta charset="UTF-8">
-    <style>
-        header {
-            background-color: teal;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-
-<body>
-    <header>
-        <h1>
-            <?= "Hello, " . htmlspecialchars($_GET['name']);
-            ?>
-        </h1>
-    </header>
-</body>
-
-</html>
+require 'index.view.php';
+?>
---
+++ index.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Document</title>
+    <meta charset="UTF-8">
+    <style>
+        header {
+            background-color: teal;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <h1>
+            <?= $greeting ?>
+        </h1>
+    </header>
+</body>
+
+</html>
