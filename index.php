e54088f9deef9300801d3fca125253785a4bb4d4
Teatoller <steven.otieno1@gmail.com>
2019-08-13 17:48:00 +0300

    (readme): implement readme file
    - add readme file
    
    [Starts #002]

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
-<?php
-echo 'Hello World!';
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <title>Document</title>
+    <meta charset="UTF-8">
+    <style>
+        header {
+            background-color: teal;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
 
+<body>
+    <header>
+        <h1>
+            <?= "Hello, " . htmlspecialchars($_GET['name']);
+            ?>
+        </h1>
+    </header>
+</body>
+
+</html>
