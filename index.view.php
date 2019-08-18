1097d8a3b52054d7977b706f5c27a4fa1b5830b5
Teatoller <steven.otieno1@gmail.com>
2019-08-18 20:50:11 +0300

    (conditional): implement conditional
    - add traditional conditional to check status
    - add html unicode to indicate status
    - refactor conditional script
    
    [Starts #007]

--- index.view.php
+++ index.view.php
@@ -26,7 +26,24 @@
                 <strong>Accountable:</strong> <?= ucfirst($task['assigned_to']); ?>;
             </li>
             <li>
-                <strong>Status:</strong> <?= ucfirst($task['completed'] ? 'complete' : 'incomplete'); ?>
+                <strong>Status:</strong>
+                <!-- option 1 -->
+                <!-- <?= ucfirst($task['completed'] ? 'complete' : 'incomplete'); ?> -->
+
+                <!-- option 2 -->
+                <!-- <?php
+                        if ($task['completed']) {
+                            echo '&#9989;';
+                        } else {
+                            echo '&#10062;';
+                        }
+                        ?> -->
+                <!-- option 3 -->
+                <?php if ($task['completed']) : ?>
+                <span class="icon" >&#9989;</span>
+                    <?php else: ?>
+                <span class="icon" >&#10062;</span>
+                <?php endif; ?>
             </li>
         </ul>
     </header>
