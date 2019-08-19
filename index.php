ba63c5fc8d3868a8ef72c6d278b0720fcc8ed2d4
Teatoller <steven.otieno1@gmail.com>
2019-08-19 21:12:19 +0300

    (class): implement php task class
    - add a task class
    - add a description method
    - var dump
    
    [Starts #008]

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
-$task = [
-    'title' => 'cut the shrub',
-    'due' => 'tomorrow',
-    'assigned_to' => 'steven',
-    'completed' => false
-];
+
+require 'functions.php';
+class Task
+{
+
+    protected $description;
+
+    protected $completed = false;
+
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
+}
+$task = new Task('Go to the store');
+
+dd($task);
 
 require 'index.view.php';
