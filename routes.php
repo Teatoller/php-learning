0f19410833b215a558c1b63f641a7e61e12e28fe
Teatoller <steven.otieno1@gmail.com>
2019-08-22 09:23:17 +0300

    (route): implement page route
    - create route file
    - add routes
    - create controller for each view
    
    [Starts #24]

---
+++ routes.php
@@ -0,0 +1,8 @@
+<?php
+
+$router->define([
+    '' => 'controllers/index.php',
+    'about' => 'controllers/about.php',
+    'about/software' => 'controllers/about-software.php',
+    'contact' => 'controllers/contact.php'
+]);
