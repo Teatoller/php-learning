9006120f1e90a1fe729e27807873c76e88a9e3fb
Teatoller <steven.otieno1@gmail.com>
2019-08-22 09:35:22 +0300

    (index): Implement index update
    - instantiate router
    - direct view uri
    - refactor code and introduce a static load function
    
    [Starts #030]

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 
 $database = require 'core/bootstrap.php';
 
-$tasks = $database->selectAll('todos');
+// $router = new Router();
 
-require 'views/index.view.php';
+// require 'routes.php';
+
+$uri = trim($_SERVER['REQUEST_URI'],'/');
+
+$router = Router::load('routes.php');
+
+require $router->direct($uri);
